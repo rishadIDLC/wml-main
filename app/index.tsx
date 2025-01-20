bcda78dae24ffab84a99152f162271e6c62eb511
Mahadi Hassan Munna <mahadimunna22@gmail.com>
2025-01-20 11:57:14 +0600

    Updated

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useLayoutEffect} from 'react';
 import {Platform, View, StyleSheet} from "react-native";
 import {WebView} from "react-native-webview";
 import {GetBranches, WebViewURL} from "@/api-requests/base-requests";
@@ -7,11 +7,11 @@ import Constants from 'expo-constants';
 const Index = () => {
     const InjectedJavascript = `const meta = document.createElement('meta'); meta.setAttribute('content', 'initial-scale=0.5, maximum-scale=0.5, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `
 
-    useEffect(() => {
-        (async() => {
+    useLayoutEffect(() => {
+        (async () => {
             await GetBranches();
         })()
-    }, [])
+    }, []);
 
     return (
         <View style={styles.container}>
