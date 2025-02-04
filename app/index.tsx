05818ea19226979bb4c0604d94bf90d0859fab21
Mahadi Hassan Munna <mahadimunna22@gmail.com>
2025-02-04 14:43:02 +0600

    Updated

--- app/index.tsx
+++ app/index.tsx
@@ -4,24 +4,23 @@ import {WebView} from "react-native-webview";
 import {GetBranches, WebViewURL} from "@/api-requests/base-requests";
 import Constants from 'expo-constants';
 import {useCameraPermissions, useMediaLibraryPermissions} from "expo-image-picker";
-import UtilityStore from "@/store/utility-store";
 import DownloadOpenPdf, {openPDF} from "@/utility/download-open-pdf";
 import * as Notifications from "expo-notifications";
 
 const Index = () => {
-    const [loaded, setLoaded] = useState(false)
+    const [Loaded, setLoaded] = useState(false);
     const InjectedJavascript = `const meta = document.createElement('meta'); meta.setAttribute('content', 'initial-scale=0.5, maximum-scale=0.5, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `
     const [status, requestPermissions] = useCameraPermissions();
     const [mediaStatus, requestMediaPermissions] = useMediaLibraryPermissions();
     const webViewRef = useRef(null);
     const [canGoBack, setCanGoBack] = useState(false);
-    const {isLoading, setIsLoading} = UtilityStore();
 
     useLayoutEffect(() => {
         (async () => {
-            setIsLoading(true);
             let success = await GetBranches();
-            success && setIsLoading(!success);
+            if(success){
+                setLoaded(true);
+            }
         })()
     }, []);
 
@@ -68,35 +67,37 @@ const Index = () => {
 
     return (
         <View style={styles.container}>
-            <WebView
+            {!Loaded ? <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <Text>Loading...</Text>
+            </View> :<WebView
                 ref={webViewRef}
                 style={{flex: 1}}
                 source={{uri: WebViewURL}}
                 injectedJavascript={InjectedJavascript}
                 onNavigationStateChange={(navState) => {
                     setCanGoBack(navState.canGoBack);
-                    setIsLoading(false);
+                    setLoaded(false);
                 }}
                 incognito={true}
                 javaScriptCanOpenWindowsAutomatically={true}
 
                 onOpenWindow={async (syntheticEvent) => {
-                    const { targetUrl } = syntheticEvent?.nativeEvent;
+                    const {targetUrl} = syntheticEvent?.nativeEvent;
                     console.log('Intercepted OpenWindow for', targetUrl)
-                    Platform.OS==="android" && await DownloadOpenPdf(targetUrl);
+                    Platform.OS === "android" && await DownloadOpenPdf(targetUrl);
                 }}
 
                 onShouldStartLoadWithRequest={(request) => {
                     if (request.url.toLowerCase().includes('get/lead/document')) {
                         console.log("PDF URL detected:", request.url);
-                        Platform.OS==="ios" &&  DownloadOpenPdf(request.url);
+                        Platform.OS === "ios" && DownloadOpenPdf(request.url);
                         return false; // Prevent WebView from opening the PDF
                     }
                     return true; // Allow WebView to load URL
                 }}
                 setSupportMultipleWindows={true}
                 setBuiltInZoomControls={false}
-            />
+            />}
         </View>
     );
 }
